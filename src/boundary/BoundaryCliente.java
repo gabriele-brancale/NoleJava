7f9a9dfb6d2d3c8b347ee1d0586493f9b6fcf4aa
gabriele-brancale <ga.brancale@studenti.unina.it>
2024-05-18 17:14:04 +0200

    Inizio sviluppo DAO

--- src/boundary/BoundaryCliente.java
+++ src/boundary/BoundaryCliente.java
@@ -1,15 +1,6 @@
 package boundary;
 
-import java.sql.Date;
-import java.sql.Time;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
-import control.GestioneNoleggio;
-import exception.OperationException;
 
 public class BoundaryCliente {
 
@@ -58,146 +49,9 @@ public class BoundaryCliente {
 				default: 
 
 					System.out.println("Opzione non valida");
-					continue;
 
 			}
 
 		}
 	}
-
-	private static void acquistaBiglietti() {
-
-		boolean inserireCarta = true;
-		ArrayList<String> results = null;
-		ArrayList<Integer> biglietti = new ArrayList<Integer>();
-		GestioneNoleggio gestioneCinema = GestioneNoleggio.getInstance();
-		String film, email = null;
-		Date data = null;
-		Time orario = null;
-		int numPosti = 0;
-		boolean inputValido = false;
-
-		try {
-
-			System.out.println("Inserisci il film");
-			film = scan.nextLine();
-
-			while (!inputValido) {
-				try {
-					System.out.println("Inserisci la data (aaaa-MM-gg)");
-					String dataTemp = scan.nextLine();
-					data = Date.valueOf(dataTemp);
-
-					System.out.println("Inserisci l'orario (HH:mm)");
-					String orarioTemp = scan.nextLine();
-					orario = new Time(new SimpleDateFormat("HH:mm").parse(orarioTemp).getTime());
-
-					inputValido = true;
-				} catch (IllegalArgumentException | ParseException iE) {
-					System.out.println("Errore nell'acquisizione di data e ora, riprovare..");
-					System.out.println();
-				}
-			}
-
-			inputValido = false;
-			while (!inputValido) {
-				try {
-					System.out.println("Inserisci il numero di posti da acquistare");
-					numPosti = Integer.parseInt(scan.nextLine());
-					inputValido = true;
-				} catch (NumberFormatException nE) {
-					System.out.println("Errore, inserire un numero valido");
-					System.out.println();
-				}
-			}
-
-			inputValido = false;
-			while (!inputValido) {
-				System.out.println("Inserisci la email");
-
-				email = scan.nextLine();
-
-				if (email.contains("@") && email.contains(".")) {
-					inputValido = true;
-				} else {
-					System.out.println("Email non valida..");
-				}
-			}
-
-			results = gestioneCinema.acquistaBiglietti(film, data, orario, numPosti, email, biglietti);
-
-			System.out.println("Prezzo: " + results.get(0) + " euro");
-			System.out.println("Digita 'S' per confermare o qualunque altro carattere per annullare..");
-			String conferma = scan.nextLine();
-
-			if (!conferma.equals("S") && !conferma.equals("s")) {
-				System.out.println("Operazione annullata..");
-				System.out.println();
-				return;
-			}
-
-			if (!results.get(1).equals("null")) {
-				System.out.println();
-				System.out.println("Trovata carta di credito per l'utente: " + email);
-				System.out.println("Digita 'S' per confermare l'utilizzo della carta ****-****-****-"
-						+ results.get(1).substring(results.get(1).length() - 4));
-
-				conferma = scan.nextLine();
-
-				if (conferma.equals("S") || conferma.equals("s"))
-					inserireCarta = false;
-			}
-
-			if (inserireCarta) {
-				inputValido = false;
-				while (!inputValido) {
-					System.out.println("Inserire il numero di carta:");
-
-					String numeroCarta = scan.nextLine();
-
-					try {
-						Long.parseLong(numeroCarta);
-
-						if (numeroCarta.length() == 16) {
-							inputValido = true;
-						} else {
-							System.out.println("Errore inserimento carta, deve essere di 16 cifre..");
-						}
-					} catch (NumberFormatException e) {
-						System.out.println("Errore inserimento carta, deve contenere solo numeri..");
-					}
-				}
-			}
-
-			System.out.println();
-			System.out.println("Pagamento in corso..");
-			TimeUnit.SECONDS.sleep(3);
-			System.out.println("Pagamento effettuato!");
-
-			// se conferma pagamento emetto biglietti altrimenti annullo
-			// assumo sempre vero il pagamento
-			if (true) {
-				gestioneCinema.emettiBiglietti(biglietti);
-			}
-			// else {
-			// gestioneCinema.annullaBiglietti(biglietti);
-			// }
-
-			System.out.println("Invio biglietti in corso..");
-			TimeUnit.SECONDS.sleep(3);
-			System.out.println("Biglietti inviati sulla mail indicata..");
-
-			System.out.println("Acquisto completato!");
-			System.out.println();
-			System.out.println();
-
-		} catch (OperationException oE) {
-			System.out.println(oE.getMessage());
-			System.out.println("Riprovare..\n");
-		} catch (Exception e) {
-			System.out.println("Unexpected exception, riprovare..");
-			System.out.println();
-		}
-	}
-
 }
---
+++ src/database/ImbarcazioneDAO.java
@@ -0,0 +1,69 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Date;
+
+import entity.EntityImbarcazione;
+import exception.DAOException;
+import exception.DBConnectionException;
+
+import java.util.ArrayList;
+
+public class ImbarcazioneDAO {
+
+    public static ArrayList<EntityImbarcazione> ricercaImbarcazioni(String tipologia, int numeroPassegeri, Date dataInizio, Date dataFine) throws DAOException, DBConnectionException{
+
+        ArrayList<EntityImbarcazione> risultato = new ArrayList<EntityImbarcazione>();
+
+		try {
+
+			Connection conn = DBManager.getConnection();
+
+			try {
+
+				String query = "SELECT * FROM IMBARCAZIONE WHERE TIPOLOGIA=? AND CAPIENZA>=? AND" 
+                                + "((DATAINIZIO>? AND DATAINIZIO>?) OR (DATAFINE<? AND DATAFINE<?))";
+
+				PreparedStatement stmt = conn.prepareStatement(query);
+
+				stmt.setString(1, tipologia);
+
+				stmt.setInt(2, numeroPassegeri);
+
+				stmt.setDate(3, dataInizio);
+                stmt.setDate(4, dataFine);
+                stmt.setDate(5, dataInizio);
+                stmt.setDate(6, dataFine);
+
+				ResultSet result = stmt.executeQuery();
+
+                while(result.next()){
+
+                    risultato.add(new EntityImbarcazione(result.getString(1), result.getString(2), result.getString(3), result.getString(4), result.getInt(5), result.getInt(6)));
+
+                }
+
+			}catch(SQLException e) {
+
+				throw new DAOException("Errore lettura proiezione");
+
+			}finally {
+
+				DBManager.closeConnection();
+
+			}
+			
+		}catch(SQLException e) {
+            
+			throw new DBConnectionException("Errore di connessione DB");
+
+		}
+
+		return risultato; 
+
+    }
+
+}
